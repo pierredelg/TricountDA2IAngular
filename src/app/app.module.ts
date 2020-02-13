7f773db6a7fdccd326ca8a8795f2fe191a4be84b
Delgrange Pierre <pierredelgrange@hotmail.fr>
2020-02-13 13:11:33 +0100

    merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 ﻿import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import {appRoutingModule} from "./app.routing";
 import {AppComponent} from "./app.component";
