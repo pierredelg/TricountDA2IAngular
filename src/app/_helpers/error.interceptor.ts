14c02bc431edb17ac54e7eed151e38738b184534
Pierre Delgrange <pierredelgrange@hotmail.fr>
2020-03-03 19:01:02 +0100

    sans console log

--- src/app/_helpers/error.interceptor.ts
+++ src/app/_helpers/error.interceptor.ts
@@ -10,11 +10,7 @@ export class ErrorInterceptor implements HttpInterceptor {
     constructor(private authenticationService: AuthenticationService) { }
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        console.log("Requete recue : ");
-        console.log("url = " + request.url);
-        if(request.body) {
-          console.log("body = " + request.body.toString());
-        }
+
         return next.handle(request).pipe(catchError(err => {
 
           //On réceptionne les reponses du serveur
@@ -22,11 +18,6 @@ export class ErrorInterceptor implements HttpInterceptor {
             if (err.status === 401) {
                 this.authenticationService.logout();
                 location.reload();
-            }
-            if (err.status === 400) {
-
-                console.log("erreur dans le body = " + err.error);
-
             }
 
             const error = err.error || err.error.message || err.statusText;
